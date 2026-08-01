6446eeb9c79d2903a6cd2945ac5977777ac19b9e
Eziokwubundu Jenissi Ezichi <eziokwubundujenissi@gmail.com>
2026-08-01 03:57:51 +0100

    D

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -17,7 +17,7 @@ export async function register(req, res) {
         const passwordHash = await bcrypt.hash(password, 10);
         const result = await pool.query(
             `INSERT INTO users(name, email, password_hash, role)
-VALUES ($1, $2, $3, $4) RETURNING id, name, email, role `, [name, email, passwordHash, role]);
+             VALUES ($1, $2, $3, $4) RETURNING id, name, email, role, created_at`, [name, email, passwordHash, role]);
 
         const user = result.rows[0];
         const token = jwt.sign({id: user.id, role:user.role, email: user.email}, process.env.JWT_SECRET, {expiresIn: '7d'})
@@ -43,10 +43,10 @@ if (!validPassword)
     return res.status(401).json({message : "Invalid email or password"});
 
 const token = jwt.sign({id:user.id, role: user.role, email: user.email}, process.env.JWT_SECRET, {expiresIn: '7d'});
-res.status(201).json({user: {
-    id: user.id, name: user.name, email: user.email, role: user.role
-    }, token,
-})
+        res.status(200).json({
+            user: { id: user.id, name: user.name, email: user.email, role: user.role, created_at: user.created_at },
+            token,
+        });
     }catch (err){
         console.error(err);
         res.status(500).json({message : 'Server Down Try again'});
